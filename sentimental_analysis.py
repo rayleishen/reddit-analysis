472693658d55c42245b32bce9d8b30774b898b7a
AK <113404480+kmkaa@users.noreply.github.com>
2023-05-14 13:57:32 -0700

    Update sentimental_analysis.py

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -1,13 +1,18 @@
-import json
-x = open('comments.json')
-  
-data = json.load(x)
-  
-for y in data['comments']:
-    print(y)
+from pprint import pprint
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import praw
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
-x.close()
-
-# with open("comments.json", mode="r") as j_object:
-#    data = json.load(j_object)
-#    print(data["karma"])
+sia= SIA()
+results = []
+df = pd.read_csv(r'C:\Users\AK\Documents\GitHub\redditAnalysis')
+for line in df:
+    pol_score=sia.polarity_scores(line)
+    pol_score['comment']=line
+    results.append(pol_score)
+pprint(results[:3], width=100)

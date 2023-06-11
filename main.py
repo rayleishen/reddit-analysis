1a1cb66d41ac1a6dd7b3edd8fc76027398516d57
Raymond Shen <Rayleishen@gmail.com>
2023-06-11 00:22:57 -0700

    build summarizer.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import webscrapper, sentimental_analysis, summarizer
 
+#variables
+url = "https://www.reddit.com/r/LeagueOfMemes/comments/146ajwe/finally_a_vayne_counter/?utm_source=share&utm_medium=web2x&context=3"
+num_top_comments = 100
+sens_vadar = 0.2
+sens_rating = 5
 
+#products
+#png of charts
+#csv of comments
+#csv of key phrases
 
-url = "###"
 
 
 if "reddit.com" in url:
@@ -15,9 +23,12 @@ else:
     print("error")
     
     
-df = sentimental_analysis.run()
+df = sentimental_analysis.run(num_top_comments, sens_vadar)
 
 
 
 sentimental_analysis.bar_graph(df)
 sentimental_analysis.pie_chart(df)
+
+
+summarizer.keywords(sens_rating)
--- summarizer.py
+++ summarizer.py
@@ -2,6 +2,14 @@ import openai
 import json
 import pandas as pd
 
+from rake_nltk import Rake
+import nltk
+
+#comments below lines out to save processing power
+nltk.download('punkt')
+nltk.download('stopwords')
+
+import re
 
 #with open('config.json') as config_file:
 #    data = json.load(config_file)
@@ -11,27 +19,32 @@ import pandas as pd
 #openai.api_key = key
 
 
-from collections import Counter
-import csv  
+
+def keywords(sens_rating):
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv('comments.csv')
+
+    # Select the second column and convert it to a text Series
+    comments_series = df.iloc[:, 1].astype(str)
+
+    # Join the text Series into a single string
+    comments_string = ' '.join(comments_series)
 
 
-# Read the CSV file into a pandas DataFrame
-df = pd.read_csv('comments.csv')
 
-# Select the second column and convert it to a text Series
-text_series = df.iloc[:, 1].astype(str)
+    clean_comments_string = re.sub(r'[^\w\s]', '', comments_string)
 
-# Join the text Series into a single string
-text_string = ' '.join(text_series)
+    r=Rake()
+    r.extract_keywords_from_text(clean_comments_string)
 
-# split() returns list of all the words in the string
-split_it = text_string.split()
-  
-# Pass the split_it list to instance of Counter class.
-Counter = Counter(split_it)
-  
-# most_common() produces k frequently encountered
-# input values and their respective counts.
-most_occur = Counter.most_common(100)
-  
-print(most_occur)
+    keywords_file = open("keywords.txt", "w")    
+        
+    for rating, keyword in r.get_ranked_phrases_with_scores():
+        if rating > sens_rating:
+            print(rating, keyword)
+            keywords_file.write(str(rating) + " " + keyword + "\n")
+            
+    keywords_file.close()
+
+
+keywords(5)
